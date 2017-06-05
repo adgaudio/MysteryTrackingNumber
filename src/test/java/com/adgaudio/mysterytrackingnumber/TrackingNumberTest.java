99e05d3eff3d2390d5fa27a19e3237436e118568
Alex Gaudio <adgaudio@gmail.com>
2017-06-05 11:45:39 -0400

    convert to gradle lib

---
+++ src/test/java/com/adgaudio/mysterytrackingnumber/TrackingNumberTest.java
@@ -0,0 +1,14 @@
+package com.adgaudio.mysterytrackingnumber;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TrackingNumberTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
