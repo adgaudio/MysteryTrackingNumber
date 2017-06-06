bd28e9ab0de5bc7034166e07f43b80b6278fc40c
Alex Gaudio <adgaudio@gmail.com>
2017-06-06 15:43:50 -0400

    usps13 uses a standard sum of products check digit algo.

--- src/main/java/com/adgaudio/mysterytrackingnumber/CheckDigitAlgorithms.java
+++ src/main/java/com/adgaudio/mysterytrackingnumber/CheckDigitAlgorithms.java
@@ -23,4 +23,24 @@ public final class CheckDigitAlgorithms {
         }
         return x == checkDigit;
     }
+    
+    public static boolean sumOfProductsWithWeightingsMod11(ArrayList<Integer> seq1, int checkDigit) {
+        int[] weightings = {8,6,4,2,3,5,9,7};
+        int x = 0;
+        int i = 0;
+        for (int v : seq1) {
+            x += v * weightings[i];
+            i += 1;
+        }
+        int r = x % 11;
+        if (r == 1
+                ) {
+            r = 0;
+        } else if (r == 0) {
+            r = 5;
+        } else {
+            r = 11 - r;
+        }
+        return r == checkDigit;
+    }
 }
--- src/main/java/com/adgaudio/mysterytrackingnumber/couriers/Usps13.java
+++ src/main/java/com/adgaudio/mysterytrackingnumber/couriers/Usps13.java
@@ -2,6 +2,8 @@ package com.adgaudio.mysterytrackingnumber.couriers;
 
 import java.util.ArrayList;
 
+import com.adgaudio.mysterytrackingnumber.CheckDigitAlgorithms;
+
 public class Usps13 extends UspsBase {
     @Override
     protected String getTrackingNumberRegex() {
@@ -15,22 +17,6 @@ public class Usps13 extends UspsBase {
 
     @Override
     protected Boolean hasValidCheckDigit(ArrayList<Integer> seq1, int checkDigit) {
-        int[] seq2 = { 8, 6, 4, 2, 3, 5, 9, 7 };
-        int x = 0;
-        int i = 0;
-        for (int v : seq1) {
-            x += v * seq2[i];
-            i += 1;
-        }
-        int r = x % 11;
-        if (r == 1
-                ) {
-            r = 0;
-        } else if (r == 0) {
-            r = 5;
-        } else {
-            r = 11 - r;
-        }
-        return r == checkDigit;
+        return CheckDigitAlgorithms.sumOfProductsWithWeightingsMod11(seq1, checkDigit);
     }
 }
