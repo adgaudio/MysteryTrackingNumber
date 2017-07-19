c31a8d7931b5429df0268ce58770478c69b20dc5
Alex Gaudio <adgaudio@gmail.com>
2017-07-18 20:06:10 -0400

    unfortunately cannot figure out a clean way to discover the json resource files stored in the jar that works on android and pc.  so I hardcoded the list...

--- src/main/java/com/adgaudio/mysterytrackingnumber/ReadJsonFiles.java
+++ src/main/java/com/adgaudio/mysterytrackingnumber/ReadJsonFiles.java
@@ -5,22 +5,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public final class ReadJsonFiles {
 	public static List<String> getResourceFiles(String path) throws IOException {
+		// TODO Hack: cannot for the life of me figure out how to 
+		// read the list of json files in Eclipse and from jar in Android.
+		// So I hardcoded it.
 		List<String> filenames = new ArrayList<>();
-
-		try (InputStream in = ReadJsonFiles.class.getResourceAsStream(path);
-				BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
-			String resource;
-
-			while ((resource = br.readLine()) != null) {
-				filenames.add(path + "/" + resource);
-			}
-		}
-
-		return filenames;
+		 filenames.addAll(Arrays.asList(
+		 "/couriers/usps.json", "/couriers/s10.json", "/couriers/dhl.json",
+		 "/couriers/misc.json", "/couriers/fedex.json"));
+		 return filenames;
 	}
 
 	static BufferedReader openFile(String filepath) {
